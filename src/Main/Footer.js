411fd1be9260c156ba9d7a37c7d77756a929f468
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-11-12 16:18:58 -0500

    footer

--- src/Main/Footer.js
+++ src/Main/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(){
     return(
         <div className="footer">
-            <p>Flatiron: Mod5 Project @ Dong Xia </p>
+            <p>Flatiron: Mod5 Project @ 2020 @ Dong Xia </p>
             <a href="https://github.com/Dong-Yi-Xia" rel="noreferrer" target="_blank">GitHub</a>
             <a href="https://www.linkedin.com/in/dong-xia-tennis-4040/" rel="noreferrer" target="_blank">Linkedin</a>
         </div>
